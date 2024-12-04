21cb044adc78e2e8ea99b0d3231e5f3da1814de1
guibarross <guilhermecbarros7@gmail.com>
2024-12-04 15:59:12 -0300

    config backend form regitro

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -4,28 +4,48 @@ namespace App\Livewire\Auth;
 
 use App\Models\User;
 use Illuminate\Contracts\View\View;
+use App\Providers\RouteServiceProvider;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Register extends Component
 {
 
-    public ?string $name;
-    public ?string $email;
-    public ?string $email_confirmation;
-    public ?string $password;
+    #[Rule(['required', 'max:255'])]
+    public ?string $name = null;
+
+    #[Rule(['required', 'email', 'max:255', 'confirmed'])]
+    public ?string $email = null;
+
+    #[Rule(['required'])]
+    public ?string $email_confirmation = null;
+
+    #[Rule(['required'])]
+    public ?string $password = null;
+
 
     public function render(): View
     {
         return view('livewire.auth.register');
     }
 
+
     public function submit(): void 
     {
-        User::query()->create([
+
+        $this->validate();
+
+      /** @var \App\Models\User $user */
+
+      $user =  User::query()->create([
 
             'name' => $this->name,
             'email' => $this->email,
             'password' => $this->password,
         ]);
+
+        auth()->login($user);
+
+        $this->redirect(RouteServiceProvider::HOME);
     }
 }
